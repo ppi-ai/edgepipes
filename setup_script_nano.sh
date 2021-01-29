3fe6a412e0960e33bfa85cb12f18264540c44094
craston <nieves.crasto@assaabloy.com>
2021-01-29 10:26:26 +0100

    Added gedit config to setup_script_nano.sh

--- setup_script_nano.sh
+++ setup_script_nano.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
 # Adding autocomplete lines to bashrc
+echo "Setting up bash"
 echo 'bind '"\e[A": history-search-backward'' >> ~/.bashrc 
 echo 'bind '"\e[B": history-search-forward'' >> ~/.bashrc 
 echo 'alias rm='rm -i'' >> ~/.bashrc 
 echo 'alias cl='clear'' >> ~/.bashrc 
 echo 'alias lo='logout'' >> ~/.bashrc 
 
+
+# Set up  gedit editor
+echo "Setting up gedit"
+gsettings set org.gnome.gedit.preferences.editor editor-font 'Monospace 14'
+gsettings set org.gnome.gedit.preferences.editor highlight-current-line true
+gsettings set org.gnome.gedit.preferences.editor display-line-numbers true
+gsettings set org.gnome.gedit.preferences.editor bracket-matching true
+gsettings set org.gnome.gedit.preferences.editor insert-spaces true
+gsettings set org.gnome.gedit.preferences.editor tabs-size uint32 4
+gsettings set org.gnome.gedit.preferences.editor auto-indent true
+gsettings set org.gnome.gedit.preferences.editor display-overview-map true
+gsettings set org.gnome.gedit.preferences.editor scheme 'oblivion'
+
 # setup git
+echo "Setting up git"
 git remote set-url origin https://github.com/ppi-ai/edgepipes.git
 git pull
 git checkout demo
 git config user.name craston
 git config user.email nieves.crasto@assaabloy.com
+
+#Change keyboard layout
+sudo dpkg-reconfigure keyboard-configuration
